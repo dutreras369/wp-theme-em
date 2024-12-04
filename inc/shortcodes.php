9dc6613e07283c9fd08848073fe8201a4ed0efec
dutreras <dutreras369@gmail.com>
2024-12-04 09:31:20 -0300

    update detailsç

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -90,7 +90,7 @@ function ev_about_shortcode() {
                 </p>
 
                 <!-- Botón de suscripción -->
-                <a href="https://calendly.com/momistica/asesoria-alquimiza" target="_blank" class="btn btn-em-dark-blue btn-lg shadow-lg">
+                <a href="https://calendly.com/momistica/asesoria-alquimiza" target="_blank" class="btn btn-em-gold btn-lg shadow-lg">
                     Agenda <i class="bi bi-calendar rounded-circle"></i>
                 </a>
             </div>
