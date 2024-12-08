b67ca172c1ade412d358932a1bb13d77a794bf66
dutreras <dutreras369@gmail.com>
2024-12-08 10:51:12 -0300

    update landing page

--- functions.php
+++ functions.php
@@ -144,7 +144,7 @@ function blog_theme_scripts() {
     wp_enqueue_style( 'bootstrap-icon', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css', array(), '1.10.5' );
 
 	// CSS de Lightbox minificado
-	wp_enqueue_style('lightbox-css', 'https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.5/css/lightbox.css', array(), '2.11.5');
+    wp_enqueue_style('lightbox-css', 'https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/css/lightbox.min.css', array(), '2.11.3');
 
 
     // Encolar jQuery correctamente (viene con WordPress)
@@ -167,7 +167,7 @@ function blog_theme_scripts() {
 	));
 
 	// JS de Lightbox minificado
-	wp_enqueue_script('lightbox-js', '	https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.5/js/lightbox.min.js', array('jquery'), '2.11.5', true);
+    wp_enqueue_script('lightbox-js', 'https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox.min.js', array('jquery'), '2.11.3', true);
 }
 add_action( 'wp_enqueue_scripts', 'blog_theme_scripts' );
 
